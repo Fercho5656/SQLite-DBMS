745d0aceb5db3d79dc6d4641d3b42d7f4db0f948
Fercho5656 <morenomoranfernando@gmail.com>
2022-03-08 14:59:47 -0600

    Method to send queries to server

--- Frontend/src/services/database.ts
+++ Frontend/src/services/database.ts
@@ -1,5 +1,4 @@
 export const uploadFile = async (file: File) => {
-  console.log('TS')
   const formData: FormData = new FormData()
   formData.append('file', file)
   try {
@@ -9,6 +8,23 @@ export const uploadFile = async (file: File) => {
       body: formData
     })
     const json = await response.json()
+    return json
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const sendQuery = async (query: string) => {
+  try {
+    const response = await fetch('http://localhost:3001/query', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ query })
+    })
+    const json = await response.json()
+    console.log('json', typeof json)
     return json
   } catch (err) {
     console.error(err)

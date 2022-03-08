1731140ec40c84780bd0ea9e7e713123ed07f146
Fercho5656 <morenomoranfernando@gmail.com>
2022-03-08 14:59:20 -0600

    Added input to send queries

--- Frontend/src/views/Upload.vue
+++ Frontend/src/views/Upload.vue
@@ -3,6 +3,10 @@
     <input type="file" @change="onFileChange" name="database" />
     <button @click="onUploadFile" :disabled="!selectedFile">Upload</button>
   </div>
+  <div class="query">
+    <input type="text" v-model="query" placeholder="SQLite Query" />
+    <button @click="onSendQuery" :disabled="!query">Send Query</button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,8 +16,11 @@ export default {
 </script>
 <script setup lang='ts'>
 import { ref } from "vue";
-import { uploadFile } from "../services/database";
+import { uploadFile, sendQuery } from "../services/database";
+
 const selectedFile = ref();
+const query = ref('');
+const result = ref();
 
 const onFileChange = (e: any) => {
   selectedFile.value = e.target.files[0];
@@ -21,8 +28,13 @@ const onFileChange = (e: any) => {
 };
 
 const onUploadFile = async () => {
-  uploadFile(selectedFile.value);
+  await uploadFile(selectedFile.value);
 };
+
+const onSendQuery = async () => {
+  result.value = sendQuery(query.value);
+  console.log(result.value)
+}
 </script>
 
 <style>

e324b3ccc395945aa6bc36362c6ace13ca68cfc7
Fercho5656 <morenomoranfernando@gmail.com>
2022-03-12 18:32:09 -0600

    Input to create database

--- Frontend/src/views/Upload.vue
+++ Frontend/src/views/Upload.vue
@@ -3,10 +3,15 @@
     <input type="file" @change="onFileChange" name="database" />
     <button @click="onUploadFile" :disabled="!selectedFile">Upload</button>
   </div>
+  <div class="create-database">
+    <input type="text" name="newDatabase" v-model="newDatabaseName" required />
+    <button @click="onCreateDatabase" :disabled="!newDatabaseName">Create</button>
+  </div>
   <div class="query">
     <input type="text" v-model="query" placeholder="SQLite Query" />
     <button @click="onSendQuery" :disabled="!query">Send Query</button>
   </div>
+  <h2>Saved Databases</h2>
 </template>
 
 <script lang="ts">
@@ -16,9 +21,10 @@ export default {
 </script>
 <script setup lang='ts'>
 import { ref } from "vue";
-import { uploadFile, sendQuery } from "../services/database";
+import { uploadFile, sendQuery, createDatabase } from "../services/database";
 
 const selectedFile = ref();
+const newDatabaseName = ref('')
 const query = ref('');
 const result = ref();
 
@@ -34,6 +40,10 @@ const onUploadFile = async () => {
 const onSendQuery = async () => {
   result.value = sendQuery(query.value);
   console.log(result.value)
+}
+
+const onCreateDatabase = async () => {
+  await createDatabase(newDatabaseName.value);
 }
 </script>
 

62a7d1f71158bb6ed3cc0245fbe01c92291deb31
Fercho5656 <morenomoranfernando@gmail.com>
2022-04-01 11:56:37 -0600

    Imported new component

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Upload from "./views/Upload.vue";
+import Manager from "./views/Manager.vue";
 </script>
 
 <template>
-  <Upload />
+  <Manager />
 </template>
 
 <style>

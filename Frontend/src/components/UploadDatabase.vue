dc49c997cb31e8ee11f05c67fe35368486b6ff56
Fercho5656 <morenomoranfernando@gmail.com>
2022-04-01 11:55:31 -0600

    Refactored upload component into its own component

---
+++ Frontend/src/components/UploadDatabase.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="file-upload">
+    <input type="file" @change="onFileChange" name="database" />
+    <button @click="onUploadFile" :disabled="!selectedFile">Upload</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "UploadDatabase",
+};
+</script>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { uploadFile } from "../services/database";
+
+const selectedFile = ref();
+
+const onFileChange = (e: any) => (selectedFile.value = e.target.files[0]);
+const onUploadFile = async () => await uploadFile(selectedFile.value);
+</script>
+
+<style>
+</style>

3482842aba1974f7af748ad614ef531fa1a55a0b
Fercho5656 <morenomoranfernando@gmail.com>
2022-05-03 18:53:53 -0500

    Added action header with edit and delete buttons

--- Frontend/src/components/Tables.vue
+++ Frontend/src/components/Tables.vue
@@ -1,42 +1,49 @@
 <template>
-    <h2>Tables</h2>
-    <button :disabled="tables.length === 0" @click="$emit('onSwitchModal', true)">Create table</button>
-    <div class="wrapper">
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Schema</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="table in tables" :key="table.name">
-                    <td>{{ table.name }}</td>
-                    <td>{{ table.sql }}</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+  <h2>Tables</h2>
+  <button :disabled="tables.length === 0" @click="$emit('onSwitchModal', true)">
+    Create table
+  </button>
+  <div class="wrapper">
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Schema</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="table in tables" :key="table.name">
+          <td>{{ table.name }}</td>
+          <td>{{ table.sql }}</td>
+          <td>
+            <button>🖊</button>
+            <button>❌</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script lang="ts">
 export default {
-    name: "Tables",
+  name: "Tables",
 };
 </script>
 
 <script setup lang="ts">
 interface Props {
-    tables: any[];
+  tables: any[];
 }
 const props = defineProps<Props>();
-const emit = defineEmits(['onSwitchModal']);
+const emit = defineEmits(["onSwitchModal"]);
 </script>
 
 <style scoped>
 .wrapper {
-    text-align: left;
-    overflow-x: auto;
-    white-space: nowrap;
+  text-align: left;
+  overflow-x: auto;
+  white-space: nowrap;
 }
 </style>

0cb8bf13a84e4ec760d6461f3b15fbe0cd21d195
Fercho5656 <morenomoranfernando@gmail.com>
2022-05-09 00:43:11 -0500

    Modal and refs for AlterTable component

--- Frontend/src/components/Tables.vue
+++ Frontend/src/components/Tables.vue
@@ -1,6 +1,9 @@
 <template>
+  <Modal :show="showAlterTable" @close="onSwitchModal(false)">
+    <AlterTable :columns="tableColumns" :tableName="selectedTable" />
+  </Modal>
   <h2>Tables</h2>
-  <button :disabled="tables.length === 0" @click="$emit('onSwitchModal', true)">
+  <button :disabled="!isDatabaseSelected" @click="$emit('onSwitchModal', true)">
     Create table
   </button>
   <div class="wrapper">
@@ -17,8 +20,7 @@
           <td>{{ table.name }}</td>
           <td>{{ table.sql }}</td>
           <td>
-            <button>🖊</button>
-            <button>❌</button>
+            <button @click="onEditTable(table.name)">🖊</button>
           </td>
         </tr>
       </tbody>
@@ -27,17 +29,41 @@
 </template>
 
 <script lang="ts">
+import { ref } from 'vue';
+
 export default {
   name: "Tables",
 };
 </script>
 
 <script setup lang="ts">
+import { sendQuery, Queries } from '../services/database';
+import Modal from './Modal.vue'
+import AlterTable from './AlterTable.vue'
+
 interface Props {
   tables: any[];
+  isDatabaseSelected: boolean;
 }
+
 const props = defineProps<Props>();
-const emit = defineEmits(["onSwitchModal"]);
+const emit = defineEmits(["onSwitchModal", "onDeleteTable"]);
+
+const tableColumns = ref([] as any[]);
+const selectedTable = ref<string>('');
+const showAlterTable = ref<boolean>(false);
+
+const onSwitchModal = (newValue: boolean) => showAlterTable.value = newValue
+const onDeleteTable = (tableName: string) => emit("onDeleteTable", tableName);
+const onEditTable = async (tableName: string) => {
+  const res = await sendQuery(Queries.describeTable.replace('%s', tableName));
+  if (res.status !== 500) {
+    tableColumns.value = res;
+    selectedTable.value = tableName;
+    showAlterTable.value = true
+  }
+}
+
 </script>
 
 <style scoped>

2e38de77d602a80e20b131493b4707699c769dad
Fercho5656 <morenomoranfernando@gmail.com>
2022-05-09 00:42:50 -0500

    AlterTable component

---
+++ Frontend/src/components/AlterTable.vue
@@ -0,0 +1,36 @@
+<template>
+    <h2>Alter {{tableName}}</h2>
+    <button>Add Column</button>
+    <table>
+        <thead>
+            <tr>
+                <th>Column</th>
+                <th>Type</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="column in columns" :key="column.name">
+                <td>{{ column.name }}</td>
+                <td>{{ column.type }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script lang="ts">import { ref } from 'vue';
+
+export default {
+    name: 'AlterTable'
+}
+</script>
+
+<script lang="ts" setup>
+interface Props {
+    columns: any[];
+    tableName: string;
+}
+const props = defineProps<Props>();
+</script>
+
+<style scoped>
+</style>

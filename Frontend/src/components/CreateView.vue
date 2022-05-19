e610e4afcda0dbffb5d93f9706cf0355077ddfc9
Fercho5656 <morenomoranfernando@gmail.com>
2022-05-18 23:33:48 -0500

    Component to create views

---
+++ Frontend/src/components/CreateView.vue
@@ -0,0 +1,42 @@
+<template>
+    <h2>Create View</h2>
+    <input type="text" v-model="viewName">
+    <SelectTable :columns="columns" :table-name="tableName" @on-send-query="onReceiveQuery" />
+    <code>
+        {{ viewQuery }}
+        <br>
+    </code>
+    <button @click="onCreateView">Create View</button>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+import SelectTable from './SelectTable.vue';
+import { sendQuery } from '../services/database';
+export default {
+    name: 'CreateView'
+}
+</script>
+
+<script lang="ts" setup>
+interface Props {
+    columns: any[];
+    tableName: string;
+}
+
+const props = defineProps<Props>();
+const viewName = ref<string>(`${props.tableName}View`);
+const viewQuery = ref<string>('');
+const onReceiveQuery = (query: string) => {
+    viewQuery.value = `CREATE VIEW ${viewName.value} AS ${query}`;
+}
+const onCreateView = async () => {
+    const res = await sendQuery(viewQuery.value);    
+    if (res) {
+        console.log(res);
+    }
+}
+</script>
+
+<style scoped>
+</style>
